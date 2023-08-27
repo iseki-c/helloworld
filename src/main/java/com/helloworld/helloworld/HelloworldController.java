2f9b7fb2a1e4911294ac96d8d8ac08cd4c1fb772
Your Name <you@example.com>
2023-08-27 13:42:20 +0900

    プロジェクトをセットアップ

---
+++ src/main/java/com/helloworld/helloworld/HelloworldController.java
@@ -0,0 +1,15 @@
+package com.helloworld.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloworldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return ("Hello World");
+    }
+}
